fc1d543e0cf199b82840d5100fc4983a0b0c1965
Greninja573 <technipump@hotmail.com>
2019-10-07 17:58:04 +0700

    Added form views and validation error

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,9 +3,14 @@
 @section('isi')
 <h1 style="text-align:center;">User Management</h1>
 <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#ModalCreateData">
+<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#ModalCreateData" style="margin-bottom: 10px;">
   Add Data
 </button>
+@if (session('status'))
+	<div class="alert alert-success">
+		{{ session('status') }}
+	</div>
+@endif
 {{-- List User --}}
 <table class="table">
     <thead class="thead-dark">
@@ -36,7 +41,7 @@
     </tbody>
 </table>
 <!-- Modal Create -->
-<div class="modal" id="ModalCreateData" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="ModalCreateData" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
@@ -46,23 +51,52 @@
         </button>
       </div>
       <div class="modal-body">
-        <form action="" method="post">
+        <form action="/" method="post">
             @csrf
             <div class="form-group">
                 <label for="">Your Name</label>
-                <input class="form-control" type="text" name="fullname" id="fullname" placeholder="Full Name">
+                <input class="form-control @error('fullname') is-invalid @enderror" type="text" name="fullname" id="fullname" placeholder="Full Name" value="{{ old('fullname') }}">
+                @error('fullname')
+                  <div class="invalid-feedback">
+                    {{ $message }}
+                  </div>
+                @enderror
             </div>
             <div class="form-group">
                 <label for="">Username</label>
-                <input class="form-control" type="text" name="username" id="username" placeholder="Username">
+                <input class="form-control" type="text" name="username" id="username" placeholder="Username" value="{{ old('username') }}">
+                @error('username')
+                  <div class="invalid-feedback">
+                    {{ $message }}
+                  </div>
+                @enderror
             </div>
             <div class="form-group">
               <label for="">Password</label>
-              <input class="form-control" type="text" name="password" id="password" placeholder="Password">
+              <input class="form-control" type="text" name="password" id="password" placeholder="Password" value="{{ old('password') }}">
+              @error('password')
+                <div class="invalid-feedback">
+                  {{ $message }}
+                </div>
+              @enderror
             </div>
             <div class="form-group">
               <label for="">E-Mail</label>
-              <input class="form-control" type="text" name="email" id="email" placeholder="E-Mail">
+              <input class="form-control" type="text" name="email" id="email" placeholder="E-Mail" value="{{ old('email') }}">
+              @error('email')
+                <div class="invalid-feedback">
+                  {{ $message }}
+                </div>
+              @enderror
+            </div>
+            <div class="form-group">
+              <label for="">Phone Number</label>
+              <input class="form-control" type="text" name="phonenumber" id="phonenumber" placeholder="Starts with +62" value="{{ old('phonenumber') }}">
+              @error('password')
+                <div class="invalid-feedback">
+                  {{ $message }}
+                </div>
+              @enderror
             </div>
             <div class="form-group">
               <label for="">Position</label>
@@ -72,12 +106,17 @@
                   <option value="{{ $position[$i]->position }}">{{ $position[$i]->position }}</option>
                 @endfor
               </select>
+              @error('position')
+                <div class="invalid-feedback">
+                  {{ $message }}
+                </div>
+              @enderror
             </div>
         </form>
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
-        <button type="submit" class="btn btn-primary">Add Data</button>
+        <input type="submit" class="btn btn-primary" value="Add Data">
       </div>
     </div>
   </div>
