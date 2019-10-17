eb92d18347475f08c4b78f9650b01eb938e38499
Greninja573 <technipump@hotmail.com>
2019-10-17 17:40:03 +0700

    Fixed append and div insert

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -12,7 +12,7 @@
 	</div>
 @endif
 {{-- List User --}}
-<table class="table table-responsive-md">
+<table class="table">
     <thead class="thead-dark">
         <tr>
             <th scope="col">ID</th>
@@ -53,9 +53,10 @@
         </button>
       </div>
       <div class="modal-body">
-        <div id="insert">
         <form action="/" method="post" id="dynamic_form">
             @csrf
+        <div id="insert">
+          <span id="result"></span>
             <div class="form-group">
                 <label for="">Your Name</label>
                 <input class="form-control @error('fullname') is-invalid @enderror" type="text" name="fullname[]" id="fullname" placeholder="Full Name" value="">
@@ -76,7 +77,11 @@
             </div>
             <div class="form-group">
               <label for="">Password</label>
-              <input class="form-control @error('password') is-invalid @enderror" type="password" name="password[]" id="password" placeholder="Password" value="">
+              <input class="form-control @error('password') is-invalid @enderror form-password" type="password" name="password[]" id="password" placeholder="Password" value="">
+              <div class="form-check">
+                <input type="checkbox" class="form-check-input form-checkbox">
+                <label for="" class="form-check-label">Show Password</label>
+              </div>
               @error('password')
                 <div class="invalid-feedback">
                   {{ $message }}
@@ -160,10 +165,76 @@ $(document).ready(function () {
     });
     $('#btnAdd').click(function () {
         i++;
-        $('#insert').append(`
-        <form action="/" method="post" id="dynamic_form_`+i+`">
-          <button type="button" id="`+i+`" class="btn btn-danger btn_close">Close Form</button>
-            @csrf
+    //     $('#insert').append(`
+    //       <button type="button" id="`+i+`" class="btn btn-danger btn_close">Close Form</button>
+    //         @csrf
+    //         <div class="form-group">
+    //             <label for="">Your Name</label>
+    //             <input class="form-control @error('fullname') is-invalid @enderror" type="text" name="fullname[]" id="fullname" placeholder="Full Name" value="">
+    //             @error('fullname')
+    //               <div class="invalid-feedback">
+    //                 {{ $message }}
+    //               </div>
+    //             @enderror
+    //         </div>
+    //         <div class="form-group">
+    //             <label for="">Username</label>
+    //             <input class="form-control @error('username') is-invalid @enderror" type="text" name="username[]" id="username" placeholder="Username" value="">
+    //             @error('username')
+    //               <div class="invalid-feedback">
+    //                 {{ $message }}
+    //               </div>
+    //             @enderror
+    //         </div>
+    //         <div class="form-group">
+    //           <label for="">Password</label>
+    //           <input class="form-control @error('password') is-invalid @enderror form-password" type="password" name="password[]" id="password" placeholder="Password" value="">
+    //           <div class="form-check">
+    //             <input type="checkbox" class="form-check-input form-checkbox">
+    //             <label for="" class="form-check-label">Show Password</label>
+    //           </div>
+    //           @error('password')
+    //             <div class="invalid-feedback">
+    //               {{ $message }}
+    //             </div>
+    //           @enderror
+    //         </div>
+    //         <div class="form-group">
+    //           <label for="">E-Mail</label>
+    //           <input class="form-control @error('email') is-invalid @enderror" type="text" name="email[]" id="email[]" placeholder="E-Mail" value="">
+    //           @error('email')
+    //             <div class="invalid-feedback">
+    //               {{ $message }}
+    //             </div>
+    //           @enderror
+    //         </div>
+    //         <div class="form-group">
+    //           <label for="">Phone Number</label>
+    //           <input class="form-control @error('phonenumber') is-invalid @enderror" type="text" name="phonenumber[]" id="phonenumber" placeholder="Starts with +62" value="">
+    //           @error('phonenumber')
+    //             <div class="invalid-feedback">
+    //               {{ $message }}
+    //             </div>
+    //           @enderror
+    //         </div>
+    //         <div class="form-group">
+    //           <label for="">Position</label>
+    //           <select class="form-control @error('position') is-invalid @enderror" name="position" id="position">
+    //             <option value="">--Select Position--</option>
+    //             @for ($i = 0; $i < count($position); $i++)
+    //               <option value="{{ $position[$i]->position }}">{{ $position[$i]->position }}</option>
+    //             @endfor
+    //           </select>
+    //           @error('position')
+    //             <div class="invalid-feedback">
+    //               {{ $message }}
+    //             </div>
+    //           @enderror
+    //         </div>
+    //   </div>
+    // `);
+    $('#insert').append(`
+            <button type="button" id="`+i+`" class="btn btn-danger btn_close">Close Form</button>
             <div class="form-group">
                 <label for="">Your Name</label>
                 <input class="form-control @error('fullname') is-invalid @enderror" type="text" name="fullname[]" id="fullname" placeholder="Full Name" value="">
@@ -184,7 +255,11 @@ $(document).ready(function () {
             </div>
             <div class="form-group">
               <label for="">Password</label>
-              <input class="form-control @error('password') is-invalid @enderror" type="password" name="password[]" id="password" placeholder="Password" value="">
+              <input class="form-control @error('password') is-invalid @enderror form-password" type="password" name="password[]" id="password" placeholder="Password" value="">
+              <div class="form-check">
+                <input type="checkbox" class="form-check-input form-checkbox">
+                <label for="" class="form-check-label">Show Password</label>
+              </div>
               @error('password')
                 <div class="invalid-feedback">
                   {{ $message }}
@@ -193,7 +268,7 @@ $(document).ready(function () {
             </div>
             <div class="form-group">
               <label for="">E-Mail</label>
-              <input class="form-control @error('email') is-invalid @enderror" type="text" name="email[]" id="email[]" placeholder="E-Mail" value="">
+              <input class="form-control @error('email') is-invalid @enderror" type="text" name="email[]" id="email" placeholder="E-Mail" value="">
               @error('email')
                 <div class="invalid-feedback">
                   {{ $message }}
@@ -211,7 +286,7 @@ $(document).ready(function () {
             </div>
             <div class="form-group">
               <label for="">Position</label>
-              <select class="form-control @error('position') is-invalid @enderror" name="position" id="position">
+              <select class="form-control @error('position') is-invalid @enderror" name="position[]" id="position">
                 <option value="">--Select Position--</option>
                 @for ($i = 0; $i < count($position); $i++)
                   <option value="{{ $position[$i]->position }}">{{ $position[$i]->position }}</option>
@@ -223,85 +298,88 @@ $(document).ready(function () {
                 </div>
               @enderror
             </div>
-      </div>
-    </form>
     `);
     });
+    $('.form-checkbox').click(function(){
+      if($(this).is(':checked')){
+        $('.form-password').attr('type','text');
+      }else{
+       $('.form-password').attr('type','password'); 
+      }
+    });
+  $('#dynamic_form').on('submit', function(event){
+                event.preventDefault();
+                $.ajaxSetup({
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    }
+                    });
+                $.ajax({
+                    url:'{{ route(".") }}',
+                    method:'post',
+                    data:$(this).serialize(),
+                    dataType:'json',
+                    beforeSend:function(){
+                        $('#save').attr('disabled','disabled');
+                    },
+                    success:function(data)
+                    {
+                        if(data.error)
+                        {
+                            var error_html = '';
+                            for(var count = 0; count < data.error.length; count++)
+                            {
+                                error_html += '<p>'+data.error[count]+'</p>';
+                            }
+                            $('#result').html('<div class="alert alert-danger">'+error_html+'</div>');
+                        }
+                        else
+                        {
+                            $('#result').html('<div class="alert alert-success">'+data.success+'</div>');
+                        }
+                        $('#save').attr('disabled', false);
+                    }
+                }) 
+    });
+});
+  //     $('#dynamic_form').submit(function(event) {
+  //      event.preventDefault();
 
-    // $('#dynamic_form').on('submit', function(event){
-    //             event.preventDefault();
-    //             $.ajaxSetup({
-    //                 headers: {
-    //                     'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-    //                 }
-    //                 });
-    //             $.ajax({
-    //                 url:'{{ route(".") }}',
-    //                 method:'post',
-    //                 data:$(this).serialize(),
-    //                 dataType:'json',
-    //                 beforeSend:function(){
-    //                     $('#save').attr('disabled','disabled');
-    //                 },
-    //                 success:function(data)
-    //                 {
-    //                     if(data.error)
-    //                     {
-    //                         var error_html = '';
-    //                         for(var count = 0; count < data.error.length; count++)
-    //                         {
-    //                             error_html += '<p>'+data.error[count]+'</p>';
-    //                         }
-    //                         $('#result').html('<div class="alert alert-danger">'+error_html+'</div>');
-    //                     }
-    //                     else
-    //                     {
-    //                         $('#result').html('<div class="alert alert-success">'+data.success+'</div>');
-    //                     }
-    //                     $('#save').attr('disabled', false);
-    //                 }
-    //             }) 
-    //     }); 
-    // });
-      $('#dynamic_form').submit(function(event) {
-       event.preventDefault();
-
-    $.ajax({
-    url: '.',
-    type: 'POST',
-    data: $(this).serialize(),
-    success: function(data){
-        alert(data);
-        // success logic
-    },
-    error: function(jqXhr, json, errorThrown){// this are default for ajax errors 
-        var errors = jqXhr.responseJSON;
-        var errorsHtml = '';
-        $.each(errors['errors'], function (index, value) {
-            errorsHtml += '<ul class="list-group"><li class="list-group-item alert alert-danger">' + value + '</li></ul>';
-        });
-        //I use SweetAlert2 for this
-        swal({
-            title: "Error " + jqXhr.status + ': ' + errorThrown,// this will output "Error 422: Unprocessable Entity"
-            html: errorsHtml,
-            width: 'auto',
-            confirmButtonText: 'Try again',
-            cancelButtonText: 'Cancel',
-            confirmButtonClass: 'btn',
-            cancelButtonClass: 'cancel-class',
-            showCancelButton: true,
-            closeOnConfirm: true,
-            closeOnCancel: true,
-            type: 'error'
-        }, function(isConfirm) {
-            if (isConfirm) {
-                 $('#openModal').click();//this is when the form is in a modal
-            }
-        });
-    };
-  });
-  });
-  )};
-)};
+  //   $.ajax({
+  //   url: '.',
+  //   type: 'POST',
+  //   data: $(this).serialize(),
+  //   success: function(data){
+  //       alert(data);
+  //       // success logic
+  //   },
+  //   error: function(jqXhr, json, errorThrown){// this are default for ajax errors 
+  //       var errors = jqXhr.responseJSON;
+  //       var errorsHtml = '';
+  //       $.each(errors['errors'], function (index, value) {
+  //           errorsHtml += '<ul class="list-group"><li class="list-group-item alert alert-danger">' + value + '</li></ul>';
+  //       });
+  //       //I use SweetAlert2 for this
+  //       swal({
+  //           title: "Error " + jqXhr.status + ': ' + errorThrown,// this will output "Error 422: Unprocessable Entity"
+  //           html: errorsHtml,
+  //           width: 'auto',
+  //           confirmButtonText: 'Try again',
+  //           cancelButtonText: 'Cancel',
+  //           confirmButtonClass: 'btn',
+  //           cancelButtonClass: 'cancel-class',
+  //           showCancelButton: true,
+  //           closeOnConfirm: true,
+  //           closeOnCancel: true,
+  //           type: 'error'
+  //       }, function(isConfirm) {
+  //           if (isConfirm) {
+  //                $('#openModal').click();//this is when the form is in a modal
+  //           }
+  //       });
+  //   },
+  // });
+  // });
+  // });
 </script>
 @endsection
